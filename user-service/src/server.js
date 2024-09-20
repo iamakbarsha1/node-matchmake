c44b310bf52d1a7e7154c55366c8b98314117556
iamakbarsha1 <iamakbarsha1@gmail.com>
2024-09-20 17:00:57 +0530

    Update cloud env for mongoDB and introduced NODE_ENV in serverless, server.js

--- user-service/src/server.js
+++ user-service/src/server.js
@@ -2,7 +2,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const UserRouter = require("./routes/userRouter");
 const ItemRouter = require("./routes/itemRouter");
-require("dotenv").config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config({
+    path: `.env.${process.env.NODE_ENV}`,
+  });
+}
 
 const port = 7000 || process.env.PORT;
 
@@ -17,10 +21,15 @@ app.get("/", (req, res) => {
 const connectToDatabase = async () => {
   if (mongoose.connection.readyState === 0) {
     try {
-      await mongoose.connect(process.env.MONGODB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      });
+      await mongoose.connect(
+        process.env.NODE_ENV !== "prod"
+          ? process.env.MONGODB_URI_LOCAL
+          : process.env.MONGODB_URI,
+        {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        }
+      );
       console.log("Connected to MongoDB");
     } catch (err) {
       console.error("Could not connect to MongoDB...", err);
@@ -36,7 +45,10 @@ app.use("/user", UserRouter);
 
 app.listen(port, (res, err) => {
   if (err) return console.log("Server Down!😲");
-  else return console.log(`Server Up & Running on http://localhost:${port}/`);
+  else {
+    console.log(`Server Up & Running on http://localhost:${port}/`);
+    console.log(`Running in ${process.env.NODE_ENV} environment`);
+  }
 });
 
 // MongoDB.connect(process.env.MONGODB_URI)
