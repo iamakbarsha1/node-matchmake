8ad45ae1404e2dc419fecfc45f41322df3bcff10
iamakbarsha1 <iamakbarsha1@gmail.com>
2024-10-07 23:34:47 +0530

    update dotenv config

--- user-service/src/server.js
+++ user-service/src/server.js
@@ -2,7 +2,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 const UserRouter = require("./routes/userRouter");
 const ItemRouter = require("./routes/itemRouter");
-require("dotenv").config();
+// require("dotenv").config();
+require("dotenv").config({
+  // path: `.env.${process.env.NODE_ENV}`,
+  path: `.env`,
+});
+
 // if (process.env.NODE_ENV !== "production") {
 //   require("dotenv").config({
 //     path: `.env.${process.env.NODE_ENV}`,
@@ -25,10 +30,10 @@ const connectToDatabase = async () => {
   if (mongoose.connection.readyState === 0) {
     try {
       await mongoose.connect(
-        process.env.MONGODB_URI,
-        // process.env.NODE_ENV !== "prod"
-        //   ? process.env.MONGODB_URI_LOCAL
-        //   : process.env.MONGODB_URI,
+        // process.env.MONGODB_URI,
+        process.env.NODE_ENV !== "prod"
+          ? process.env.MONGODB_URI_LOCAL
+          : process.env.MONGODB_URI,
         {
           useNewUrlParser: true,
           useUnifiedTopology: true,
